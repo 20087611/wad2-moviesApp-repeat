d502c6b9fb48e9ac45f90f4eca8ef5c23b81540e
Daijie Liu <20087611@mail.wit.ie>
2022-08-22 18:14:19 +0100

    Start CA-1

--- src/contexts/moviesContext.js
+++ src/contexts/moviesContext.js
@@ -5,30 +5,39 @@ export const MoviesContext = React.createContext(null);
 const MoviesContextProvider = (props) => {
   const [myReviews, setMyReviews] = useState( {} ) 
   const [favorites, setFavorites] = useState( [] )
-  const [playlist, setPlaylist] = useState([]);
+  const [playlist, setPlaylist] = useState( [] );
 
+
+  // add movies to favorite list
   const addToFavorites = (movie) => {
     let newFavorites = [];
     if (!favorites.includes(movie.id)){
       newFavorites = [...favorites, movie.id];
+    } else{
+      newFavorites = favorites;
     }
     setFavorites(newFavorites)
   };
-  // We will use this function in a later section
   const removeFromFavorites = (movie) => {
     setFavorites( favorites.filter(
       (mId) => mId !== movie.id
     ) )
   };
 
+
+  // add reviews to movie
   const addReview = (movie, review) => {
     setMyReviews( {...myReviews, [movie.id]: review } )
   };
 
+
+  // add upcoming movies to playlist
   const addToPlaylist = (movie) => {
     let newPlaylist = [];
     if (!playlist.includes(movie.id)) {
       newPlaylist = [...playlist, movie.id];
+    } else {
+      newPlaylist = playlist;
     }
     setPlaylist(newPlaylist);
   };
@@ -36,9 +45,6 @@ const MoviesContextProvider = (props) => {
     setPlaylist( playlist.filter(
       (mId) => mId !== movie.id
     ) )
-    console.log(playlist.filter(
-      (mId) => mId !== movie.id
-    ));
   };
 
   return (
