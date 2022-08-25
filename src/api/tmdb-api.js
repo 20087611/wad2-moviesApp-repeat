7ac89b1d52498e5c6599615ee4a91396abcd9ac4
Daijie Liu <20087611@mail.wit.ie>
2022-08-25 01:48:04 +0100

    Integrate with movies and persons API

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -1,15 +1,24 @@
+// export const getMovies = () => {
+//   return fetch(
+//     `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+//   ).then((response) => {
+//     if (!response.ok) {
+//       throw new Error(response.json().message);
+//     }
+//     return response.json();
+//   })
+//     .catch((error) => {
+//       throw error
+//     });
+// };
+
 export const getMovies = () => {
   return fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
-  ).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.json().message);
+     '/api/movies',{headers: {
+       'Authorization': window.localStorage.getItem('token')
     }
-    return response.json();
-  })
-    .catch((error) => {
-      throw error
-    });
+  }
+  ).then(res => res.json());
 };
 
 export const getUpcomingMovies = () => {
@@ -129,18 +138,28 @@ export const getMovieReviews = (id) => {
     });
 };
 
+// export const getPersons = () => {
+//   return fetch(
+//     `https://api.themoviedb.org/3/person/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
+//   ).then((response) => {
+//     if (!response.ok) {
+//       throw new Error(response.json().message);
+//     }
+//     return response.json();
+//   })
+//   .catch((error) => {
+//      throw error
+//   });
+// };
+
 export const getPersons = () => {
   return fetch(
-    `https://api.themoviedb.org/3/person/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
-  ).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.json().message);
+    '/api/persons', {
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
     }
-    return response.json();
-  })
-  .catch((error) => {
-     throw error
-  });
+  }
+  ).then(res => res.json());
 };
 
 export const getPerson = (args) => {
